4905dd35e3115c4a433ec8503737e29a25b62ad4
Ginaldo-Silva <138523497+Ginaldo-Silva@users.noreply.github.com>
2023-07-07 15:55:57 -0300

    FInalização tela de login!
    Chegamos a uma etapa final da tela de login, estamos sujeitos a novas mudanças no design da página!

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function togglePasswordVisibility() {
-    var passwordInput = document.getElementById("password");
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-    } else {
-      passwordInput.type = "password";
-    }
+
+  var imgElement =  document.getElementById('passwordImage');
+  var currentImage = imgElement.getAttribute('src');
+  var newImage =  '';
+
+  if(currentImage === 'img/EyeClossed.png')
+  {
+    newImage = 'img/EyeOpen.png';
+  } else {
+    newImage = 'img/EyeClossed.png';
   }
+
+  imgElement.setAttribute('src', newImage);
+
+  imgElement.setAttribute('src', newImage);
+  var passwordInput = document.getElementById("password");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+}
